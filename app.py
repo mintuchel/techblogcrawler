7bfd541480ab639f22d77a45676275308cdf958c
mintuchel <max3627@naver.com>
2025-03-14 10:49:05 +0900

    refactor: packaging for deployment

---
+++ app.py
@@ -0,0 +1,10 @@
+# import streamlit as st
+# import streamlit.components.v1 as components
+
+# st.title("Hello, Streamlit! 🚀")
+
+# # HTML 파일을 읽어와서 표시
+# with open("views/index.html", "r", encoding="utf-8") as f:
+#     html_content = f.read()
+
+# st.components.v1.html(html_content, height=1000, scrolling=True)
